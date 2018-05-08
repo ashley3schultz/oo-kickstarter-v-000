f104c638247fbd160d7a5aee6932f80b3abe46a4
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-08 02:41:30 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,8 +14,8 @@ class Backer
   
   def back_project(project_name)
     @backed_projects << project_name
-    @backed_projects.detect {|proj| proj.title == project_name.title}
-      binding.pry
+    @backed_projects.detect {|proj| proj.title == project_name.title
+      binding.pry}
     
      
   end
