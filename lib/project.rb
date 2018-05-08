c7226784b17bcad37a546288cf872671eb61bb64
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-08 02:45:29 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,4 @@ class Project
     @backers << backer
     @backers.detect {|b| b.backed_projects << self if b.name == backer.name}
   end
-  
-      @backed_projects << project_name
-    @backed_projects.detect {|proj| proj.backers << self if proj.title == project_name.title}
 end
